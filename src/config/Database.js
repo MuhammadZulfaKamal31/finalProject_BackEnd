82c000f8f0915ee3532933df1e9a97ccfe8df692
Muhammad Zulfa Kamal <zulfakamal019@gmail.com>
2023-06-05 19:05:01 +0700

    set deploy2

--- src/config/Database.js
+++ src/config/Database.js
@@ -1,12 +1,11 @@
 const { Sequelize } = require("sequelize");
 const dotenv = require("dotenv")
-dotenv.config({ path: "./env" })
+dotenv.config({ path: "./.env" })
 
-//set up deploy di railway agar bisa terhubung di database dan portnya harus sama di railway
-const db = new Sequelize(process.env.NAME_DB, process.env.USER_DB, process.env.PASSWORD_DB, {
+const db = new Sequelize('postgres', 'postgres', 'Agustus31', {
     host: process.env.HOST_DB,
     dialect: 'postgres',
     port: process.env.PORT_DB
-});
+})
 
 module.exports = db;
