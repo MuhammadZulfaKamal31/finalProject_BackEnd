05d597fc58b9d6cc9e4fed15d361133145d2b643
Muhammad Zulfa Kamal <zulfakamal019@gmail.com>
2023-06-05 21:06:23 +0700

    migrasi databse

--- src/config/Database.js
+++ src/config/Database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 const dotenv = require("dotenv")
 dotenv.config({ path: "./.env" })
 
-const db = new Sequelize(process.env.NAME_DB, process.env.USER_DB, 'Agustus@31', {
+const db = new Sequelize("crud_db", 'postgres', 'Agustus@31', {
     host: process.env.HOST_DB,
     dialect: 'postgres',
     port: process.env.PORT_DB
